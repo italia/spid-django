30e0082c51ac7f5570aa0cb707ab82677c089fed
guglielmo <guglielmo.celata@gmail.com>
2017-10-07 18:30:07 +0200

    login page works

---
+++ demo/views.py
@@ -0,0 +1,116 @@
+from django.conf import settings
+from django.urls import reverse
+from django.http import (HttpResponse, HttpResponseRedirect,
+                         HttpResponseServerError)
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+from onelogin.saml2.auth import OneLogin_Saml2_Auth
+from onelogin.saml2.settings import OneLogin_Saml2_Settings
+from onelogin.saml2.utils import OneLogin_Saml2_Utils
+
+
+def init_saml_auth(req):
+    auth = OneLogin_Saml2_Auth(req, custom_base_path=settings.SAML_FOLDER)
+    return auth
+
+
+def prepare_django_request(request):
+    # If server is behind proxys or balancers use the HTTP_X_FORWARDED fields
+    result = {
+        'https': 'on' if request.is_secure() else 'off',
+        'http_host': request.META['HTTP_HOST'],
+        'script_name': request.META['PATH_INFO'],
+        'server_port': request.META['SERVER_PORT'],
+        'get_data': request.GET.copy(),
+        # Uncomment if using ADFS as IdP, https://github.com/onelogin/python-saml/pull/144
+        # 'lowercase_urlencoding': True,
+        'post_data': request.POST.copy()
+    }
+    return result
+
+
+def index(request):
+    req = prepare_django_request(request)
+    auth = init_saml_auth(req)
+    errors = []
+    not_auth_warn = False
+    success_slo = False
+    attributes = False
+    paint_logout = False
+
+    if 'sso' in req['get_data']:
+        return HttpResponseRedirect(auth.login())
+    elif 'sso2' in req['get_data']:
+        return_to = OneLogin_Saml2_Utils.get_self_url(req) + reverse('attrs')
+        return HttpResponseRedirect(auth.login(return_to))
+    elif 'slo' in req['get_data']:
+        name_id = None
+        session_index = None
+        if 'samlNameId' in request.session:
+            name_id = request.session['samlNameId']
+        if 'samlSessionIndex' in request.session:
+            session_index = request.session['samlSessionIndex']
+
+        return HttpResponseRedirect(auth.logout(name_id=name_id, session_index=session_index))
+    elif 'acs' in req['get_data']:
+        auth.process_response()
+        errors = auth.get_errors()
+        not_auth_warn = not auth.is_authenticated()
+        if not errors:
+            request.session['samlUserdata'] = auth.get_attributes()
+            request.session['samlNameId'] = auth.get_nameid()
+            request.session['samlSessionIndex'] = auth.get_session_index()
+            if 'RelayState' in req['post_data'] and OneLogin_Saml2_Utils.get_self_url(req) != req['post_data']['RelayState']:
+                return HttpResponseRedirect(auth.redirect_to(req['post_data']['RelayState']))
+    elif 'sls' in req['get_data']:
+        dscb = lambda: request.session.flush()
+        url = auth.process_slo(delete_session_cb=dscb)
+        errors = auth.get_errors()
+        if len(errors) == 0:
+            if url is not None:
+                return HttpResponseRedirect(url)
+            else:
+                success_slo = True
+
+    if 'samlUserdata' in request.session:
+        paint_logout = True
+        if len(request.session['samlUserdata']) > 0:
+            attributes = request.session['samlUserdata'].items()
+
+    context = RequestContext(request, {'errors': errors,
+                                       'not_auth_warn': not_auth_warn,
+                                       'success_slo': success_slo,
+                                       'attributes': attributes,
+                                       'paint_logout': paint_logout}).flatten()
+    return render_to_response('index.html',
+                              context=context)
+
+
+def attrs(request):
+    paint_logout = False
+    attributes = False
+
+    if 'samlUserdata' in request.session:
+        paint_logout = True
+        if len(request.session['samlUserdata']) > 0:
+            attributes = request.session['samlUserdata'].items()
+
+    return render_to_response('attrs.html',
+                              context=RequestContext(request, {'paint_logout': paint_logout,
+                                                               'attributes': attributes}).flatten())
+
+
+def metadata(request):
+    # req = prepare_django_request(request)
+    # auth = init_saml_auth(req)
+    # saml_settings = auth.get_settings()
+    saml_settings = OneLogin_Saml2_Settings(settings=None, custom_base_path=settings.SAML_FOLDER, sp_validation_only=True)
+    metadata = saml_settings.get_sp_metadata()
+    errors = saml_settings.validate_metadata(metadata)
+
+    if len(errors) == 0:
+        resp = HttpResponse(content=metadata, content_type='text/xml')
+    else:
+        resp = HttpResponseServerError(content=', '.join(errors))
+    return resp
