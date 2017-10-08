a8f41de32d0dfce1d6a729ffe0b27fc8ff8cb468
Azzeccagarbugli <f.coppola1998@gmail.com>
2017-10-08 07:34:50 +0200

    themes -- updated the theme for Sphinx

--- django_spid/doc/conf.py
+++ django_spid/doc/conf.py
@@ -17,9 +17,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- General configuration ------------------------------------------------
@@ -31,6 +31,7 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+
 extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
@@ -85,9 +86,26 @@ todo_include_todos = False
 #
 # html_theme = 'alabaster'
 
-import sphinx_rtd_theme
-html_theme = "sphinx_rtd_theme"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme = 'sphinx_italia_theme'
+html_theme_path = ["_themes", ]
+
+
+# -- ReadTheDoc requirements and local template generation---------------------
+
+# on_rtd is whether we are on readthedocs.org, this line of code grabbed from docs.readthedocs.org
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    html_theme = 'sphinx_italia_theme'
+    html_theme_path = ["_themes", ]
+else:
+    # Override default css to get a larger width for ReadTheDoc build
+    html_context = {
+        'css_files': [
+            '_static/css/theme.css',
+            '_static/css/custom.css',
+        ],
+    }
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
