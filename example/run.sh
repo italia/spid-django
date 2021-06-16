fa6c86bbdadb702f6a823bdea0ce3c030b9e9405
peppelinux <giuseppe.demarco@unical.it>
2021-06-16 16:25:44 +0200

    feat: uwsg debug server with http keepalive

--- example/run.sh
+++ example/run.sh
@@ -2,5 +2,4 @@ python -B ./manage.py migrate
 python -B ./manage.py collectstatic --noinput
 
 # python -B ./manage.py runserver 0.0.0.0:8000
-uwsgi --https 0.0.0.0:8000,./certificates/public.cert,./certificates/private.key --module example.wsgi:application --env example.settings --chdir .
-
+uwsgi --http-keepalive  --https 0.0.0.0:8000,./certificates/public.cert,./certificates/private.key --module example.wsgi:application --env example.settings --chdir .
