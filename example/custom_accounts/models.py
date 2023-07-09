ed9afd408092ebbd7351be3f6acf3b37dcea6b75
rglauco <37829079+rglauco@users.noreply.github.com>
2023-07-09 21:26:42 +0200

    fix: updated django and requirements

--- example/custom_accounts/models.py
+++ example/custom_accounts/models.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 
 _custom_perm =  (
--- example/custom_accounts/views.py
+++ example/custom_accounts/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import get_object_or_404
 from .models import *
 from .forms import *
 
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from django.core.exceptions import ValidationError
 
 from django.template import RequestContext
