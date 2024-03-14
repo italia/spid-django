ccd7f840ac30087137cb2d096949543e639a0803
Simone Del Zotto <delzotto@infofactory.it>
2024-03-14 12:14:43 +0100

    Add User-Agent heder to avoid 403 Forbidden error when downloading metadata from poste.it

--- src/djangosaml2_spid/management/commands/update_idps.py
+++ src/djangosaml2_spid/management/commands/update_idps.py
@@ -64,7 +64,7 @@ class Command(BaseCommand):
                 indentation_level=1,
             )
 
-            #Add User-Agent heder to avoid 403 Forbidden error when downloading metada from poste.it
+            #Add User-Agent heder to avoid 403 Forbidden error when downloading metadata from poste.it
             with requests.get(idp_metadata_url, verify=True, headers={'User-Agent': ''}) as response:
                 identity_provider["metadata"] = response.text
 
